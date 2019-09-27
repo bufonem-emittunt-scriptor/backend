e7a8b337a4f6c791f1187bd24e131d4aad84a2e7
Сергей Белан <sbelan@metaprime.ru>
2019-09-27 21:59:47 +0300

    вывод моделей в админку

--- backend/apps/museer/admin.py
+++ backend/apps/museer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+
+from .models import *
 # Register your models here.
+
+admin.site.register(Platform)
+admin.site.register(Volunteer)
+admin.site.register(News)
+admin.site.register(BlackList)
+admin.site.register(Contacts)
+admin.site.register(Event)
+admin.site.register(EventSubscription)
+admin.site.register(Marks)
+admin.site.register(PlatformReviews)
+admin.site.register(VolunteerReviews)
